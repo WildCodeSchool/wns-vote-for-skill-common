f0a961502b3c1f7df3421f6afe65531510075a13
Thomas Culdaut <thomas.culdaut@gmail.com>
2021-02-26 11:05:06 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import BadRequestError from './errors/BadRequestError';
-import InputError from './errors/InputError';
-import NotFoundError from './errors/NotFoundError';
-
 export * from './middlewares/error';
-export * as BadRequestError from './errors/BadRequestError';
-export * as CustomError from './errors/CustomError';
-export * as InputError from './errors/InputError';
 export * from './errors/NotFoundError';
